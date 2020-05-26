a42c8d2c565a382a24f8849924db65d6646cb849
idith almog <almog.idith@gmail.com>
2020-05-26 19:34:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
-  # your code here
+  attr_accessor :status, :sender, :receiver, :amount
+  def initialize(sender,receiver,amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
 end
