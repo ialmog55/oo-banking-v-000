396f977a1836c1bb1d510c53a5d0e7cb54395e5b
idith almog <almog.idith@gmail.com>
2020-05-26 19:53:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,16 @@ class Transfer
     end
   end
   def execute_transaction
-    if self.valid? && @sender.balance > amount && @status != "complete"
-      @sender.balance -= amount
-      @receiver.balance += amount
+    if self.valid? && @sender.balance > @amount && @status != "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status ="complete"
     else
       return "Transaction rejected. Please check your account balance."
     end
-
+  end
+  def reverse_transfer
+    if @status == "complete"
+      @sender
   end
 end
