a50aa03cad7f21bc1348b21c54940396899831b2
idith almog <almog.idith@gmail.com>
2020-05-26 19:49:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.balance += amount
       @status ="complete"
     else
-      return "transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end
 
   end
